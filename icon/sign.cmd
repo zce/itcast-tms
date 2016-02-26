9fe910eed50bb4080b3041298d75064a28add0f9
iceStone <ice@micua.com>
2016-02-26 10:12:32 +0800

    remove useless file

---
+++ icon/sign.cmd
@@ -0,0 +1,43 @@
+@REM @Author: iceStone
+
+@REM @Date:   2016-02-20 15:28:51
+
+@REM @Last Modified by:   iceStone
+
+@REM Modified time: 2016-02-20 15:30:00
+
+
+
+.\rcedit "itcast-evaluation.exe" --set-icon "app.ico"
+
+
+.\rcedit "itcast-evaluation.exe" --set-version-string "ProductVersion" "2.2.0"
+
+
+.\rcedit "itcast-evaluation.exe" --set-version-string "ProductName" "Itcast Evaluation"
+
+
+.\rcedit "itcast-evaluation.exe" --set-version-string "OriginalFilename" "evaluation.exe"
+
+
+.\rcedit "itcast-evaluation.exe" --set-version-string "InternalName" "evaluation.exe"
+
+
+.\rcedit "itcast-evaluation.exe" --set-version-string "LegalCopyright" "Copyright (C) 2016 Itcast, Inc. All rights reserved."
+
+
+.\rcedit "itcast-evaluation.exe" --set-version-string "FileDescription" "传智播客教学测评系统"
+
+ 
+.\rcedit "itcast-evaluation.exe" --set-version-string "CompanyName" "Itcast, Inc."
+
+
+.\rcedit "itcast-evaluation.exe" --set-product-version "2.2.0"
+
+
+.\rcedit "itcast-evaluation.exe" --set-file-version "2.2.0"
+
+
+.\rcedit "itcast-evaluation.exe" --set-version-string "Comments" "传智播客教学测评系统"
+
+pause
