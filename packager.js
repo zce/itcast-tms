687e8816b258c8b4c8c51c27edd5347af4484989
iceStone <ice@micua.com>
2016-04-24 22:28:46 +0800

    restructuring done

--- packager.js
+++ packager.js
@@ -27,24 +27,20 @@ const OPTIONS = {
   'name': packageJson.productName,
   'build-version': packageJson.version,
   'icon': app_icon,
-  'ignore': file => {
-    console.log(file);
-    return true;
-  },
-  // [
-  //   /.git/,
-  //   /dist/,
-  //   /log/,
-  //   /[^core/]node_modules/,
-  //   /src/,
-  //   /temp/,
-  //   /test/,
-  //   /.DS_Store/,
-  //   /.gitignore/,
-  //   /gulpfile.js/,
-  //   /packager.js/,
-  //   /README.md/
-  // ],
+  'ignore': [
+    /.git/,
+    /^\/dist/,
+    /^\/log/,
+    /^\/node_modules/,
+    /^\/src/,
+    /^\/temp/,
+    /^\/test/,
+    /.DS_Store/,
+    /.gitignore/,
+    /gulpfile.js/,
+    /packager.js/,
+    /README.md/
+  ],
   'overwrite': true,
   // prune: true,
   'version': '0.37.6',
